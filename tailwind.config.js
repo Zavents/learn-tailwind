c9573e615d7b6c1a7afaee61b2898062518691ae
zaven <pku.mca16@gmail.com>
2024-10-03 14:53:30 +0700

    lot of input.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  important: true,
-  content: ['./src/**/*.{html,js,css}'],
+  content: ['./**/*.{html,js}'],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
+
